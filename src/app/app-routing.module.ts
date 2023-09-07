1c9f65b4b1ea0bcf46c58bd0c9230f51136e8a26
Mostafa Ali <mostafaali789@gmail.com>
2023-09-07 06:12:09 +0300

    2nd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:"home" , pathMatch:"full"},
+  {path:"home", component:HomeComponent},
+  {path:"about", component:AboutComponent},
+  {path:"portfolio", component:PortfolioComponent},
+  {path:"contact", component:ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
